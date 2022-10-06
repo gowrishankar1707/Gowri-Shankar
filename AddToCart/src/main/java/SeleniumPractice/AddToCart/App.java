c596f79b58c73dcef32eeb60d16e129db77950ad
Gowrishankar <57629071+gowrishankar1707@users.noreply.github.com>
2022-10-06 21:30:54 +0530

    exec:java

--- AddToCart/src/main/java/SeleniumPractice/AddToCart/App.java
+++ AddToCart/src/main/java/SeleniumPractice/AddToCart/App.java
@@ -19,35 +19,39 @@ public class App
 {
     public static void main( String[] args ) throws Exception
     {
-    	String[] itemsNeeded= {"Brinjal","Capsicum"};
-    	
-    	//button[text()="ADD TO CART"]
-        System.setProperty("webdriver.chrome.driver", "D:\\\\ChromeDriver\\\\chromedriver.exe");
-        
-        WebDriver driver= new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-        WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(30));
-        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
-        List<WebElement> listOfproducts= driver.findElements(By.cssSelector("h4[class='product-name']"));
-        int count=0;
-        
-       for(int i=0;i<listOfproducts.size();i++)
-       {
-    	   String[] productName=listOfproducts.get(i).getText().split(" - ", 2);
-    	   List<String> productsNeeded=Arrays.asList(itemsNeeded);
-    	   if(productsNeeded.contains(productName[0]))
-    	   {
-    		   System.out.println(i);
-   		   driver.findElements(By.cssSelector(".product-action button[type='button']")).get(i).click();
-//  not suggested    		   driver.findElement(By.xpath("//div[@class='product']/h4[contains(text(),"+listOfproducts.get(i).getText()+")]/parent::div/div[@class='product-action']/button")).click();
-//  not suggested  		   wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//button[contains(text(),'ADDED')]")));
-    		   ++count;
-    		   if(count == productsNeeded.size())
-    			   break;
-    	   }
-    	   
-       }
+    	if(args.length > 0)
+    	{
+    		System.out.println(args[0]+"....."+args[1]);
+    	}
+//    	String[] itemsNeeded= {"Brinjal","Capsicum"};
+//    	
+//    	//button[text()="ADD TO CART"]
+//        System.setProperty("webdriver.chrome.driver", "D:\\\\ChromeDriver\\\\chromedriver.exe");
+//        
+//        WebDriver driver= new ChromeDriver();
+//        driver.manage().window().maximize();
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+//        WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(30));
+//        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+//        List<WebElement> listOfproducts= driver.findElements(By.cssSelector("h4[class='product-name']"));
+//        int count=0;
+//        
+//       for(int i=0;i<listOfproducts.size();i++)
+//       {
+//    	   String[] productName=listOfproducts.get(i).getText().split(" - ", 2);
+//    	   List<String> productsNeeded=Arrays.asList(itemsNeeded);
+//    	   if(productsNeeded.contains(productName[0]))
+//    	   {
+//    		   System.out.println(i);
+//   		   driver.findElements(By.cssSelector(".product-action button[type='button']")).get(i).click();
+////  not suggested    		   driver.findElement(By.xpath("//div[@class='product']/h4[contains(text(),"+listOfproducts.get(i).getText()+")]/parent::div/div[@class='product-action']/button")).click();
+////  not suggested  		   wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//button[contains(text(),'ADDED')]")));
+//    		   ++count;
+//    		   if(count == productsNeeded.size())
+//    			   break;
+//    	   }
+//    	   
+//       }
         
     }
 }
--- AngularPracticeSelenium/src/main/java/assignment2/AngularPracticeSelenium/App.java
+++ AngularPracticeSelenium/src/main/java/assignment2/AngularPracticeSelenium/App.java
@@ -17,33 +17,38 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.setProperty("webdriver.chrome.driver", "D:\\ChromeDriver\\chromedriver.exe");
-        WebDriver driver=new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
-        driver.get("https://rahulshettyacademy.com/angularpractice/");
-        Assert.assertEquals(driver.findElement(By.cssSelector("a[class='navbar-brand']")).getText(), "ProtoCommerce");
-        driver.findElement(By.cssSelector("input[name='name'][class*='form-control']")).sendKeys("Gowrishankar");
-        driver.findElement(By.cssSelector("input[name='email'][class*='form-control']")).sendKeys("abcd@gmail.com");
-        driver.findElement(By.cssSelector("input[type='password']")).sendKeys("shankar@1707");
-        driver.findElement(By.id("exampleCheck1")).click();
-        WebElement gender=driver.findElement(By.id("exampleFormControlSelect1"));
-        Select selectOption=new Select(gender);
-//        selectOption.selectByIndex(1);
-        selectOption.selectByVisibleText("Female");
-        Assert.assertEquals(selectOption.getFirstSelectedOption().getText(), "Female");
-        driver.findElement(By.cssSelector("input[value='option1']")).click();
-        driver.findElement(By.cssSelector("input[name='bday']")).sendKeys("17-02-2000");
-        driver.findElement(By.cssSelector("input[value='Submit']")).click();
-        if(driver.findElement(By.cssSelector(".alert.alert-success.alert-dismissible")).getText().contains("Success"))
-        {
-        	System.out.println(driver.findElement(By.cssSelector(".alert.alert-success.alert-dismissible")).getText());
-        	Assert.assertTrue(true);
-        }
-        else
-        {
-        	Assert.assertTrue(false);
-        }
+    	if(args.length > 0)
+    	{
+    		System.out.println(args[0]);
+    	}
+    	
+//        System.setProperty("webdriver.chrome.driver", "D:\\ChromeDriver\\chromedriver.exe");
+//        WebDriver driver=new ChromeDriver();
+//        driver.manage().window().maximize();
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+//        driver.get("https://rahulshettyacademy.com/angularpractice/");
+//        Assert.assertEquals(driver.findElement(By.cssSelector("a[class='navbar-brand']")).getText(), "ProtoCommerce");
+//        driver.findElement(By.cssSelector("input[name='name'][class*='form-control']")).sendKeys("Gowrishankar");
+//        driver.findElement(By.cssSelector("input[name='email'][class*='form-control']")).sendKeys("abcd@gmail.com");
+//        driver.findElement(By.cssSelector("input[type='password']")).sendKeys("shankar@1707");
+//        driver.findElement(By.id("exampleCheck1")).click();
+//        WebElement gender=driver.findElement(By.id("exampleFormControlSelect1"));
+//        Select selectOption=new Select(gender);
+////        selectOption.selectByIndex(1);
+//        selectOption.selectByVisibleText("Female");
+//        Assert.assertEquals(selectOption.getFirstSelectedOption().getText(), "Female");
+//        driver.findElement(By.cssSelector("input[value='option1']")).click();
+//        driver.findElement(By.cssSelector("input[name='bday']")).sendKeys("17-02-2000");
+//        driver.findElement(By.cssSelector("input[value='Submit']")).click();
+//        if(driver.findElement(By.cssSelector(".alert.alert-success.alert-dismissible")).getText().contains("Success"))
+//        {
+//        	System.out.println(driver.findElement(By.cssSelector(".alert.alert-success.alert-dismissible")).getText());
+//        	Assert.assertTrue(true);
+//        }
+//        else
+//        {
+//        	Assert.assertTrue(false);
+//        }
         
         
         
