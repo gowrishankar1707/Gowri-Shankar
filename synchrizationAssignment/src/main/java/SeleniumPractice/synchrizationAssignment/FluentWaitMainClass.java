c51dee27761e12267fa3ffb37933432ec9d86008
Gowrishankar <57629071+gowrishankar1707@users.noreply.github.com>
2022-10-08 22:28:55 +0530

    fluentwait

---
+++ synchrizationAssignment/src/main/java/SeleniumPractice/synchrizationAssignment/FluentWaitMainClass.java
@@ -0,0 +1,43 @@
+package SeleniumPractice.synchrizationAssignment;
+
+import java.time.Duration;
+import java.util.function.Function;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+public class FluentWaitMainClass {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "D:\\ChromeDriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+		By startButton=By.cssSelector("[id='start'] button");
+		driver.findElement(startButton).click();
+		
+		Wait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(20)).pollingEvery(Duration.ofSeconds(2));
+		
+		WebElement e=wait.until(new Function<WebDriver,WebElement>() {
+			public WebElement apply(WebDriver driver) {
+				WebElement hw=driver.findElement(By.cssSelector("[id='finish']"));
+				String hwStyle=hw.getCssValue("display");
+				System.out.println(hwStyle);
+				if(hwStyle.contains("none"))
+					return null;
+				else
+					return driver.findElement(By.cssSelector("[id='finish'] h4"));
+				
+			}
+
+		});
+		
+		System.out.println(e.getText());
+
+	}
+
+}
